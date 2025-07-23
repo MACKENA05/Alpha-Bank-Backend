3d95702bb9b551f045670ea69e4f3f7b36426fe9
MACKENA05 <mackenamuthui@gmail.com>
2025-07-23 22:34:17 +0300

    first commit

---
+++ src/main/java/com/mackena/Banking_Application_backend/BankingApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mackena.Banking_Application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationBackendApplication.class, args);
+	}
+
+}
