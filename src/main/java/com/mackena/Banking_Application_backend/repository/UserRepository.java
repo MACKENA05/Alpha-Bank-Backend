003f163ba32e5e4d43953e512d76643c3a7b80f8
MACKENA05 <mackenamuthui@gmail.com>
2025-07-24 11:27:16 +0300

    create Account repository

--- src/main/java/com/mackena/Banking_Application_backend/repository/UserRepository.java
+++ src/main/java/com/mackena/Banking_Application_backend/repository/UserRepository.java
@@ -2,25 +2,30 @@ package com.mackena.Banking_Application_backend.repository;
 
 import com.mackena.Banking_Application_backend.models.entity.User;
 import com.mackena.Banking_Application_backend.models.enums.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    Authentication
+    //    Authentication using email and phonenumber during signUp and signIn
+
     Optional<User> findByEmail(String username);
 
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
 
-//    User Management
+//    User Management based on roles/active
 
     List<User> findByRole(UserRole role);
 
@@ -45,5 +50,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // Admin queries
     @Query("SELECT u FROM User u WHERE u.role = 'ADMIN' AND u.isActive = true")
     List<User> findActiveAdmins();
+
+    @Query("SELECT u FROM User u ORDER BY u.createdAt DESC")
+    Page<User> findRecentUsers(Pageable pageable);
 
 }
